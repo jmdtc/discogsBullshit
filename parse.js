ce10f3e817c280744ce0830e513c7d41d386a639
jmdtc <mathieudelisle@outlook.com>
2019-09-28 15:01:17 +0200

    updated

--- parse.js
+++ parse.js
@@ -2,15 +2,24 @@ let fs = require('fs')
 let es = require('event-stream')
 const conv = require('xml-js');
 
-fs.createReadStream('discogs.xml', {start:11, encoding: "utf8"}) //, {start: 0, end:100}
-  .pipe(es.split())
+const releases = []
+
+fs.createReadStream('discogs.xml', {start:11, end: 20000}) //, {start: 0, end:100}
+  .pipe(es.split('</release>'))
   .pipe(es.mapSync((line) => {
-    //result = conv.xml2js(line, {compact:true, alwaysArray: true, ignoreComment: true, spaces: 2})
-    console.log(typeof line)
+    result = conv.xml2js(line + '</release>', {compact:true, ignoreComment: true, spaces: 2})
     //console.log("hello", "\n")
-    //console.log(typeof result.release)
+    if (Array.isArray(result.release.formats.format.descriptions.description)
+    && (result.release.formats.format.descriptions.description.some(el => el._text.includes('12"'))
+    || result.release.formats.format.descriptions.description.some(el => el._text.includes('10"')))) {
+      //console.log(result.release.formats.format.descriptions)
+      releases.push("success")
+    }
   })
   .on('error', (err) => {
-    console.log(err)
+    res.end(err)
+  })
+  .on('end', () => {
+    console.log("test");
   })
 )

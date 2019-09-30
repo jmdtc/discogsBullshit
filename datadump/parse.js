1dd01cdec840c442240486c65055db3487b451c7
Antoine Guilbaud <antoine.guilbaud.ext@corp.ovh.com>
2019-09-30 12:54:15 +0200

    parser

--- datadump/parse.js
+++ datadump/parse.js
@@ -2,28 +2,37 @@ let fs = require('fs')
 let es = require('event-stream')
 const conv = require('xml-js');
 
-const releases = []
-
-let stream = fs.createReadStream('discogs.xml', {start:11, end: 110000, autoDestroy: true}) //, {start: 0, end:100}
-
+let stream = fs.createReadStream('discogs.xml', {start:11, autoDestroy: true}) //, {start: 0, end:100}
+console.log("[");
+let isFirst = true
 stream.pipe(es.split('</release>'))
 .pipe(es.mapSync(function call(line) {
   result = conv.xml2js(line + '</release>', {compact:true, ignoreComment: true, spaces: 2})
-  //console.log("hello", "\n")
-  if (Array.isArray(result.release.formats.format.descriptions.description)
+  if (result.release.formats.format.hasOwnProperty('descriptions')
+  && Array.isArray(result.release.formats.format.descriptions.description)
   && (result.release.formats.format.descriptions.description.some(el => el._text.includes('12"'))
   || result.release.formats.format.descriptions.description.some(el => el._text.includes('10"')))) {
-    releases.push(result.release)
-    //console.log(result.release.formats.format.descriptions, result.release._attributes.id);
+    if (!isFirst) {
+      console.log(',');
+    }
+    console.log(JSON.stringify(result.release));
+    //releases.push(result.release)
+    //console.log(result.release);
   }
-  else if (result.release.formats.format.descriptions.description.hasOwnProperty('_text')
-  && result.release.formats.format.descriptions.description._text) {
-    releases.push(result.release)
+  else if (result.release.formats.format.hasOwnProperty('descriptions')
+  && result.release.formats.format.descriptions.description.hasOwnProperty('_text')
+  && result.release.formats.format.descriptions.description._text.includes('12"')) {
+    if (!isFirst) {
+      console.log(',');
+    }
+    console.log(JSON.stringify(result.release));
+    //releases.push(result.release)
   }
+  isFirst = false
 })
 .on('error', (err) => {
-  console.log(JSON.stringify(releases));
   //console.log(err);
+  console.log("]");
 })
 .on('end', () => {
   //console.log(JSON.stringify(releases));
